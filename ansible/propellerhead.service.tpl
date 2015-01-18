920aa9d9c0a2baf99f15570125b9f716ceda28be
Josh Butts <josh@joshbutts.com>
2015-01-18 12:46:52 -0600

    add logging for tty

--- ansible/propellerhead.service.tpl
+++ ansible/propellerhead.service.tpl
@@ -4,6 +4,7 @@ After=network.target
 
 [Service]
 Type=oneshot
+WorkingDirectory=/root/ph
 ExecStart=/root/ph/propellerhead /dev/ttyUSB0
 
 [Install]
--- src/propellerhead.go
+++ src/propellerhead.go
@@ -12,12 +12,15 @@ func main() {
 
 	if (len(os.Args) > 1) {
 		ttyPath := os.Args[1];
+		propellerhead.Logger().Info("attempting to listen for ius on " + ttyPath)
 		if _, err := os.Stat(ttyPath); err == nil {
 			wait.Add(1)
 			propellerhead.IbusDevices().SerialInterface.Listen(ttyPath)
+		} else {
+			propellerhead.Logger().Warn(ttyPath + " not found, skipping ibus")
 		}
 	}
-	
+
 	wait.Add(1)
 	propellerhead.NewAudioController()
 
